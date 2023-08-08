fe9e8ea95ed86f8e64353a99525f692683364ba9
candra <candraherdiansyah14@gmail.com>
2023-08-08 09:39:33 +0700

    axios vuex example

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
+<script>
+import Navbar from './components/Navbar.vue';
+</script>
 <template>
   <div id="nav">
+    <Navbar />
     <router-link to="/">Home</router-link> |
+    <router-link to="/users">User</router-link> |
   </div>
   <router-view />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "../views/Home.vue";
+import User from "../views/User.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/users",
+    name: "User",
+    component: User,
   },
 ];
 
